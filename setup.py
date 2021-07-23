965290892ccb8a8cdf118887ebbc1e1c71b37ad3
Bartek Kocięcki <bartosz.kociecki@gmail.com>
2021-07-23 08:22:05 +0200

    update

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ssqaapitest',
+    version='1.0',
+    description="Practice API testing",
+    author='Bartek',
+    author_email='bartkowy.k@gmail.com',
+    url='https://supersqa.com',
+    packages=find_packages()
+    )
+
